695ba882c0955cb401d4bed4287a0378afac9651
kfirmanty <karol.firmanty@gmail.com>
2021-12-01 05:53:03 +0700

    added possibility to select scales in notes view

--- lib/views/notes/melodic.lua
+++ lib/views/notes/melodic.lua
@@ -8,7 +8,9 @@ local melodic_view = {
     push = nil,
     selected_step = 1,
     base_note = 36,
-    scale = scales.minor
+    scale = scales.minor,
+    scale_options = {"chromatic", "major", "minor"},
+    selected_scale = 3
 }
 
 function melodic_view:redraw()
@@ -23,6 +25,11 @@ function melodic_view:redraw()
   end
 end
 
+function melodic_view:display_scale_info()
+  push_utils.text(self.push, "Scale:", 3, 52)
+  push_utils.text(self.push, self.scale_options[self.selected_scale] .. "  ", 4, 52)
+end
+
 function melodic_view:init(state, push, track_id, pattern_id)
     self.state = state
     self.track_id = track_id
@@ -30,6 +37,7 @@ function melodic_view:init(state, push, track_id, pattern_id)
     self.push = push
     push_utils.clear_notes(self.push)
     self:redraw()
+    self:display_scale_info()
     return self
 end
 
@@ -40,6 +48,13 @@ function melodic_view:on_click(display, event)
         self.state:edit_step(self.track_id, self.pattern_id, self.selected_step, "note", note)
         self:redraw()
         return "SELECTED_NOTE_CHANGED"
+    elseif event.type == "cc" and event.cc == 77 then
+      local change = push_utils.enc_to_inc(event.val)
+      self.selected_scale = math.max(((self.selected_scale + change) % (#self.scale_options + 1)), 1)
+      print("selected scale: " .. self.selected_scale)
+      self.scale = scales[self.scale_options[self.selected_scale]]
+      self:display_scale_info()
+      self:redraw()
     end
 end
 
